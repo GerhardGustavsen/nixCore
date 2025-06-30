068df7a0a97b2393fb6ce36be57078828b3aa363
Gerhard Gustavsen <gerhard.gustavsen@outlook.com>
2025-06-30 22:47:45 +0200

    2025-06-30 22:47:45

--- scripts/blurlock.sh
+++ scripts/blurlock.sh
@@ -7,7 +7,7 @@ RADIUS=0x${1:-2}
 IMG=/tmp/screenshot.png
 
 # Capture screen with maim and blur it
-dunstify -u normal -t 1000 -h string:fgcolor:#00ffff "🔒 Locking Computer..."
+dunstify -u normal -t 700 -h string:fgcolor:#00ffff "🔒 Locking Computer..."
 maim -u | magick - -scale 20% -blur "$RADIUS" -resize 500% "$IMG"
 
 # Lock screen with blurred image
