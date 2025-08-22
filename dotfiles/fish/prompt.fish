1de36942ae34ea2a762c6de477efd488d4a64bc9
Gerhard Gustavsen <gerhard.gustavsen@outlook.com>
2025-08-22 17:54:49 +0200

    fish

--- dotfiles/fish/prompt.fish
+++ dotfiles/fish/prompt.fish
@@ -1,14 +1,33 @@
 function fish_prompt
+    # Figure out the last path 
     set raw_dir (prompt_pwd)
     if test "$raw_dir" = /
         set last_dir /
     else
         set last_dir (string split "/" $raw_dir)[-1]
     end
-    printf '\033[38;5;93m[\033[0m'
-    printf '\033[38;5;14m%s\033[0m' $last_dir
-    printf '\033[38;5;93m]\033[0m'
-    printf '\033[38;5;14m❯ \033[0m'
+
+    if set -q SSH_CONNECTION
+        # SSH mode: big red banner + host + server IP
+        set host (hostname)
+        set parts (string split ' ' -- $SSH_CONNECTION)  # client_ip client_port server_ip server_port
+        set server_ip $parts[3]
+
+        # Red background, white text banner
+        printf '\033[48;5;196m\033[38;5;231m 🔒 SSH %s (%s) \033[0m ' $host $server_ip
+
+        # Directory + prompt in high-contrast colors
+        printf '\033[38;5;208m[\033[0m'       # orange bracket
+        printf '\033[38;5;220m%s\033[0m' $last_dir
+        printf '\033[38;5;208m]\033[0m'
+        printf '\033[38;5;220m❯ \033[0m'
+    else
+        # Your original local prompt
+        printf '\033[38;5;93m[\033[0m'
+        printf '\033[38;5;14m%s\033[0m' $last_dir
+        printf '\033[38;5;93m]\033[0m'
+        printf '\033[38;5;14m❯ \033[0m'
+    end
 end
 
 # OLD PROMT:
